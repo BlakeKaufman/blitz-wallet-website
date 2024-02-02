ad6c62589e830bd8cf1b2ad18c2c56c078dd30fd
Blake Kaufman <blakekaufman9@gmail.com>
2024-02-02 13:20:01 -0500

    homepage styles mostly complete

--- src/assets/pages/home/home.js
+++ src/assets/pages/home/home.js
@@ -1,1 +1,22 @@
-console.log("test");
+"use strict";
+
+const hamburgerMenuBTN = document.querySelector(".mobileNavBTN");
+const mobileNavContent = document.querySelector(".mobileNavDropdown");
+const closeMobileNavBTN = document.querySelector(".closeMobileNavBTN");
+
+function handleMobileNavClick() {
+  console.log(window.scrollY);
+  if (mobileNavContent.style.display === "flex") {
+    mobileNavContent.style.display = "none";
+
+    document.body.style.overflow = "auto";
+  } else {
+    mobileNavContent.style.display = "flex";
+    mobileNavContent.style.top = `${window.scrollY}px`;
+    document.body.style.overflow = "hidden";
+  }
+}
+
+[hamburgerMenuBTN, closeMobileNavBTN].forEach((btn) => {
+  btn.addEventListener("click", handleMobileNavClick);
+});
