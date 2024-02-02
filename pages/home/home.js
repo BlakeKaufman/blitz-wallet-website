d28495504b4055cd6ede8b9eae61b8b1e3575c00
Blake Kaufman <blakekaufman9@gmail.com>
2024-02-02 14:38:33 -0500

    homepage done

--- pages/home/home.js
+++ pages/home/home.js
@@ -8,12 +8,8 @@ function handleMobileNavClick() {
   console.log(window.scrollY);
   if (mobileNavContent.style.display === "flex") {
     mobileNavContent.style.display = "none";
-
-    document.body.style.overflow = "auto";
   } else {
     mobileNavContent.style.display = "flex";
-    mobileNavContent.style.top = `${window.scrollY}px`;
-    document.body.style.overflow = "hidden";
   }
 }
 
